c872172ad45c55fdc98ae4c339f5b237fae2e552
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-04-17 16:06:43 +0200

    first commit

---
+++ src/test/java/dk/sdu/messageservice/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
