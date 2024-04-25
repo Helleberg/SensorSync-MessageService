89c39c7f0f348c9a7333f7036972689a4bfe9bb6
Magnus Quist <cloudquist@gmail.com>
2024-04-25 14:39:18 +0200

    updated uuid

--- src/main/java/dk/sdu/messageservice/request_types/UpdateFirmwareRequest.java
+++ src/main/java/dk/sdu/messageservice/request_types/UpdateFirmwareRequest.java
@@ -13,6 +13,6 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UpdateFirmwareRequest {
     public String toit_firmware_version;
-    public String uuid;
+    public UUID uuid;
     public String jwt;
 }
